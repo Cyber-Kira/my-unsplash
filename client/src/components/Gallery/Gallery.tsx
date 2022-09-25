81612555cc996a7d9499b4e7968867f7d8875d50
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-09-25 14:00:35 +0300

    feat: add masonry layout for gallery

--- client/src/components/Gallery/Gallery.tsx
+++ client/src/components/Gallery/Gallery.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
+import Masonry from 'react-masonry-css'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import { fetchImages } from '../../features/file/fileSlice'
+
+const breakpointColumnsObj = {
+	default: 3,
+	500: 1,
+}
 
 export const Gallery = () => {
 	const { images } = useAppSelector(store => store.file)
@@ -11,21 +17,24 @@ export const Gallery = () => {
 	}, [])
 
 	return (
-		<div
-			className='grid md:grid-cols-3 gap-3 xl:gap-x-[2.9063rem] xl:gap-y-[2.875rem] mt-2'
-			data-packery='{ "itemSelector": ".grid-item", "gutter": 10 }'
-		>
-			{images.map(image => {
-				return (
-					<figure className='grid-item' key={`${image.id}`}>
-						<img
-							className='rounded-2xl'
-							src={`${image.url}`}
-							alt={`${image.label}`}
-						/>
-					</figure>
-				)
-			})}
+		<div className='mt-2'>
+			<Masonry
+				breakpointCols={breakpointColumnsObj}
+				className='my-masonry-grid'
+				columnClassName='my-masonry-grid_column'
+			>
+				{images.map(image => {
+					return (
+						<figure key={`${image.id}`}>
+							<img
+								className='rounded-2xl'
+								src={`${image.url}`}
+								alt={`${image.label}`}
+							/>
+						</figure>
+					)
+				})}
+			</Masonry>
 		</div>
 	)
 }
